5a763cde64a25f8fc3eeec3f5d261b32fe40222b
thetommedia@gmail.com <thetommedia@gmail.com>
2022-08-31 20:59:43 +0100

    Initial commit

--- components/Footer.js
+++ components/Footer.js
@@ -40,7 +40,7 @@ const Footer = () => {
                         <h5 className="py-1">Make money on S-Shop</h5>
                      </div>
                      <div className="footer-links">
-                        <Link href="/">
+                        <Link href="/sellingdashboard">
                            <strong className="my-0">Sell on S-Shop</strong>
                         </Link>
                      </div>
---
+++ components/SellingDashboardComponents/SellingDashboardShowcase.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const SellingDashboardShowcase = () => {
+   return (
+      <div className="selling-dashboard-showcase section">
+         <div className="container">
+            <div className="wrapper">
+               <div className="box">
+                  <h3>Make money selling on S-Shop</h3>
+                  <h5 className="py-1">Make money, money, make money</h5>
+                  <button className="btn btn-secondary">Sign up</button>
+               </div>
+               <div className="box img">
+                  <img
+                     src="https://ng.jumia.is/unsafe/fit-in/500x500/filters:fill(white)/product/09/205144/1.jpg?0768"
+                     alt=""
+                  />
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default SellingDashboardShowcase;
