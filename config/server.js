03e9ce5ff03867bf20767cbc78dc4bad705cb7b8
thetommedia@gmail.com <thetommedia@gmail.com>
2022-10-17 00:02:43 +0100

    Yes Added payment

--- config/server.js
+++ config/server.js
@@ -1,2 +1,2 @@
-// export const server = 'https://s-s-h-o-p.herokuapp.com';
-export const server = 'http://localhost:5000';
+export const server = 'https://s-s-h-o-p.herokuapp.com';
+// export const server = 'http://localhost:5000';
